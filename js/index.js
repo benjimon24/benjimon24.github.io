a3b2c15ee05b0020508cdbd9e121eedd4f7781b5
benjimon24 <benji102491@gmail.com>
2018-05-15 16:20:39 -0400

    changed colors of sidebar

--- js/index.js
+++ js/index.js
@@ -1,39 +1,46 @@
-$(document).ready(function(){
-  $(".nav-link, .text-link").on('click', function(event) {
-    if (this.hash !== "") {
+$(document).ready(function() {
+  $('.nav-link, .text-link').on('click', function(event) {
+    if (this.hash !== '') {
       event.preventDefault();
       var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 300, function(){
-        window.location.hash = hash;
-      });
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        300,
+        function() {
+          window.location.hash = hash;
+        }
+      );
     }
-  })
+  });
 
-  $("#returnButton").on('click', function(){
-    $('html, body').animate({scrollTop: 0}, 300);
-  })
+  $('#returnButton').on('click', function() {
+    $('html, body').animate({ scrollTop: 0 }, 300);
+  });
 
-  window.onscroll = function(){displayButton()};
+  window.onscroll = function() {
+    displayButton();
+  };
 
   function displayButton() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      $("#returnButton").fadeIn();
-      $(".background-text").fadeOut();
+      $('#returnButton').fadeIn();
+      // $('#content').fadeIn();
+      $('.background-text').fadeOut();
     } else {
-      $("#returnButton").fadeOut();
-      $(".background-text").fadeIn();
+      $('#returnButton').fadeOut();
+      // $('#content').fadeOut();
+      $('.background-text').fadeIn();
     }
   }
 
-  $(".modal-img").on('click', function(){
-    $("#modal").css("display", "block");
-    $("#modal-image").attr("src", $(this).attr("src"))
-  })
+  $('.modal-img').on('click', function() {
+    $('#modal').css('display', 'block');
+    $('#modal-image').attr('src', $(this).attr('src'));
+  });
 
-  $("#modal").on('click', function(){
-    $("#modal").css("display", "none");
-  })
-
+  $('#modal').on('click', function() {
+    $('#modal').css('display', 'none');
+  });
 });
