793e20e8adb0a648a23009335419d12ce5f6cba1
Benjamin Li <benjamin@dv01.co>
2025-06-25 17:10:34 -0400

    add typescript, attempting to fix console error

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/benjimon24.github.io/',
+  base: '/benjimon24.github.io',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
-  }
+  },
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.[jt]sx?$/,
+  },
 })
